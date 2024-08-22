52cdb633a958fac4fa1d3bbee5a3e7a997eab881
Gillan Flaherty <flahertygillian@gmail.com>
2024-08-22 10:42:12 -0500

    using a signal in the investment service

--- 01-starting-project/src/app/investment.service.ts
+++ 01-starting-project/src/app/investment.service.ts
@@ -1,18 +1,16 @@
-import { Injectable } from "@angular/core";
+import { Injectable, signal } from "@angular/core";
 
 import { type InvestmentInput } from "./investment-input.model";
 
 @Injectable({providedIn: "root"})
 export class InvestmentService {
-    resultData?: {
-        year: number;
-        interest: number;
-        valueEndOfYear: number;
-        annualInvestment: number;
-        totalInterest: number;
-        totalAmountInvested: number;
-      }[];
-
+    resultData = signal<{
+      year: number;
+      interest: number;
+      valueEndOfYear: number;
+      totalInterest: number;
+      totalAmountInvested: number;
+    }[] | undefined>(undefined);
 
     calculateInvestmentResults(data: InvestmentInput) {
         const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
@@ -35,7 +33,6 @@ export class InvestmentService {
           });
         }
       
-        // this.resultsData.set(annualData);
-        this.resultData = annualData;
+        this.resultData.set(annualData);
       }
 }
